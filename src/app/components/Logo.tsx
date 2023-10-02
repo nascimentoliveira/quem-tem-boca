13eccacd3bd5cccd928be9842c08fa3c9dbf5090
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-10-02 15:47:58 -0300

    refactor: change logo directory

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-import styles from "../../page.module.css";
+import styles from '../page.module.css';
 
 const Logo = () => {
   return (
