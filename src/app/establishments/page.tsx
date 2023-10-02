02af01495369a38c46ddb904700371f8326ce5a9
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-10-02 15:43:58 -0300

    feat: establishments page

--- src/app/establishments/page.tsx
+++ src/app/establishments/page.tsx
@@ -1,7 +1,34 @@
+'use client';
+import { Box, Container, Grid } from '@mui/material';
+
+import theme, { ThemeWrapper } from '../themes/theme';
+import EstablishmentCard from '../components/Establishments/EstablishmentCard';
+import Bar from '../components/Establishments/AppBar';
+import MobileBar from '../components/Establishments/MobileBar';
+import EstablishmentMenu from '../types/EstablishmentMenu';
+import mockData from '../utils/mockData';
 
 const Establishments = () => {
+  const establishments: EstablishmentMenu[] = mockData;
+
   return (
-    <p>establishments</p>
+    <ThemeWrapper>
+      <main style={{ width: '100vw' }}>
+        <Box width="100%" sx={{ backgroundColor: theme.palette.primary.main }}>
+          <MobileBar />
+          <Bar />
+          <Container maxWidth="lg" sx={{ py: 10, px: 4 }}>
+            <Grid container spacing={2}>
+              {establishments.map((establishment) => (
+                <Grid item xs={12} sm={6} md={4} key={establishment.id}>
+                  <EstablishmentCard establishment={establishment} />
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </Box>
+      </main>
+    </ThemeWrapper>
   );
 };
 
